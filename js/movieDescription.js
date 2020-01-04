e0e4619fd85dd064b9962d5ff20ab21a5ec279a2
M1Bren <blaw908@gmail.com>
2020-01-03 20:42:01 -0500

    update

--- js/movieDescription.js
+++ js/movieDescription.js
@@ -4,30 +4,24 @@ function descriptionBuilder() {
     d3.json(path)
         .then(d => {
             let data = d[0];
-            console.log(data);
+            console.log(data.Poster);
 
-            let title = d[0].Title;
-            let year = d[0].Year;
-            let rated = d[0].Rated;
-            let runtime = d[0].Runtime;
-            let genre = d[0].Genre;
-            let poster = d[0].Poster;
-            let director = d[0].Director;
-            let actors = d[0].Actors;
-            let plot = d[0].Plot;
+            // let selection = d3.select("#poster")
+            //     .selectAll(".Div")
+            //     .data(data);
 
             // Put in the Poster
-            d3.select(".img-rounded")
-                .selectAll("div")
+            d3.select("#poster")
+                .selectAll("Div")
                 .data(data)
                 .enter()
                 .append("div")
-                .classed("col-md-4", true)
-                .html(data => `<img src=${data.Poster}
-                    alt="No Movie Poster Avaliable"
-                    style="width: 18rem">
-                    </img>`
-                );
+                .classed("row img-rounded", true)
+                .html(data =>
+                    `<img class="rounded" 
+                    src=${data.Poster}
+                    style="width: 18rem"
+                    alt="No Movie Poster Avaliable">`);
         });
 };
 
