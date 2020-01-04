6e69d4c2f60bc517ed25ebaaf90a20c493e5fe40
M1Bren <blaw908@gmail.com>
2020-01-04 02:22:49 -0500

    update

--- static/js/movieDescription.js
+++ static/js/movieDescription.js
@@ -1,29 +1,29 @@
-let path = "../data/dummydata.json"
+// init();
 
-function descriptionBuilder() {
-    d3.json(path)
-        .then(d => {
-            let data = d[0];
-            console.log(data.Poster);
+// function descriptionBuilder() {
+//     d3.json(path)
+//         .then(d => {
+//             let data = d[0];
+//             console.log(data.Poster);
 
-            // document.getElementById("Poster").innerHTML = "";
+//             // document.getElementById("Poster").innerHTML = "";
 
-            // let selection = d3.select("#poster")
-            //     .selectAll(".Div")
-            //     .data(data);
+//             // let selection = d3.select("#poster")
+//             //     .selectAll(".Div")
+//             //     .data(data);
 
-            // Put in the Poster
-            d3.select("#poster")
-                .selectAll(".img-poster")
-                .data(data)
-                .enter()
-                .append("div")
-                .html(data =>
-                    `<img class="rounded" 
-                    src=${data.Poster}
-                    style="width: 18rem"
-                    alt="No Movie Poster Avaliable">`);
-        });
-};
+//             // Put in the Poster
+//             d3.select("#poster")
+//                 .selectAll(".img-poster")
+//                 .data(data)
+//                 .enter()
+//                 .append("div")
+//                 .html(data =>
+//                     `<img class="rounded" 
+//                     src=${data.Poster}
+//                     style="width: 18rem"
+//                     alt="No Movie Poster Avaliable">`);
+//         });
+// };
 
-descriptionBuilder();
+// descriptionBuilder();
