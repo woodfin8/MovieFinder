968dabf9a8ebbf02629328ddfac5f0ce95dfdcff
M1Bren <blaw908@gmail.com>
2020-01-07 23:27:38 -0500

    update

--- static/js/movieDescription.js
+++ static/js/movieDescription.js
@@ -1,20 +1,18 @@
 function movieDescription(urlFilm) {
-    console.log(urlFilm);
+
     d3.json(urlFilm)
         .then(data => {
-            console.log(data);
+            console.log(data.Poster);
 
-            document.getElementsByClassName(".img-poster").innerHTML = "";
+            // Deletes all of the child nodes in the div with img-poster class
+            d3.select(".img-poster").html("");
 
-            let selection = d3.select(".img-poster")
-                .selectAll(".Div")
-                .data(data);
 
-            d3.select("#poster")
-                .selectAll(".img-poster")
+
+            d3.select(".img-poster")
                 .data(data)
                 .enter()
-                .append("div")
+                .insert("img")
                 .html(data =>
                     `<img class="rounded" 
                     src=${data.Poster}
